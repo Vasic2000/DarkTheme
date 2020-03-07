a9b66a276aa296e0138a2734a6c6000f6c7dfa96
Vasic2000 <vasic2000@yandex.ru>
2020-03-07 23:23:55 +0300

    Doesn't work ((

--- app/src/main/java/su/vasic2000/darktheme/MainActivity.kt
+++ app/src/main/java/su/vasic2000/darktheme/MainActivity.kt
@@ -1,12 +1,45 @@
 package su.vasic2000.darktheme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme)
+        }
+        else
+            setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switch: Switch = findViewById(R.id.MySwitch)
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            switch.isChecked = true
+        }
+
+        switch.setOnCheckedChangeListener { compoundButton: CompoundButton, _: Boolean ->
+            if(compoundButton.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                restartApp()
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                restartApp()
+            }
+        }
+    }
+
+    private fun restartApp() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
